9512214499a392aa8529381c6f4c2dd866b44f3b
SeoHoJun <ghwns6659@naver.com>
2020-12-03 22:17:24 +0900

    SpringBoot_JPA Practice

--- src/main/java/jpabook/jpashop_practice/HelloController.java
+++ src/main/java/jpabook/jpashop_practice/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 	@GetMapping("hello")
 	public String hello(Model model){
 		// Model : 스프링 ui 에 있는 Model 에 데이터를 실어서 뷰로 넘겨줄 수 있다.
-		model.addAttribute("data", "hello!!!");
+		model.addAttribute("data", "hello!!");
 		return "hello"; // 화면 이름
 	}
 }
