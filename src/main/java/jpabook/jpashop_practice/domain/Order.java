8e4824230e17158d49c5b7f4601b5fd9549c7f5f
SeoHoJun <ghwns6659@naver.com>
2020-12-24 17:58:32 +0900

    SpringBoot_JPA Practice

--- src/main/java/jpabook/jpashop_practice/domain/Order.java
+++ src/main/java/jpabook/jpashop_practice/domain/Order.java
@@ -18,14 +18,14 @@ public class Order {
 	@Column(name = "order_id")
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
 
-	@OneToMany(mappedBy = "order")
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
 	private List<OrderItem> orderItems = new ArrayList<>();
 
-	@OneToOne
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "delivery_id")
 	private Delivery delivery;
 
@@ -33,4 +33,22 @@ public class Order {
 
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status; // 주문 상태(ORDER, CANCEL)
+
+	//== 연관관계 편의 메소드
+	public void setMember(Member member){
+		this.member = member;
+		member.getOrders().add(this);
+		// 양방향 연관관계 에서 데이터를 저장할 때 정방향,역방향 모두 동시에 데이터를
+		// 저장해주는 메소드를 연관관계 편의 메소드로서 생성해준다.
+	}
+
+	public void addOrderItem(OrderItem orderItem){
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+	}
+
+	public void setDelivery(Delivery delivery){
+		this.delivery = delivery;
+		delivery.setOrder(this);
+	}
 }
