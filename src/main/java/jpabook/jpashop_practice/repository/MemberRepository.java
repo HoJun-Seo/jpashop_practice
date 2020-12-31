512e400cb11f4be028609e032a6691dd0eae3ba4
SeoHoJun <ghwns6659@naver.com>
2021-01-01 00:29:53 +0900

    SpringBoot_JPA Practice

--- src/main/java/jpabook/jpashop_practice/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop_practice/repository/MemberRepository.java
@@ -7,7 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Repository
+@Repository // Spring Bean 으로 등록한다.(어노테이션을 까보면 @Component 어노테이션으로 컴포넌트의 대상임을 지정하고 있다.)
 public class MemberRepository {
 
 	@PersistenceContext
@@ -32,6 +32,13 @@ public class MemberRepository {
 		// ctrl + alt + n 키를 통해 위의 코드들을 한 줄의 리턴문으로 합쳐줄 수 있다.
 
 		return em.createQuery("select m from Member m", Member.class)
+				.getResultList();
+	}
+
+	// 만약 이름으로 멤버를 검색할 경우
+	public List<Member> findByName(String name){
+		return em.createQuery("select m from Member m where m.name = :name", Member.class)
+				.setParameter("name", name)
 				.getResultList();
 	}
 }
