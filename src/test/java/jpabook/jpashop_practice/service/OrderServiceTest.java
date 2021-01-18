3e6669842ed3ab973cbeff5653ad87d58d9fff1f
SeoHoJun <ghwns6659@naver.com>
2021-01-18 17:30:38 +0900

    SpringBoot_JPA Practice

--- src/test/java/jpabook/jpashop_practice/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop_practice/service/OrderServiceTest.java
@@ -8,8 +8,6 @@ import jpabook.jpashop_practice.domain.item.Book;
 import jpabook.jpashop_practice.domain.item.Item;
 import jpabook.jpashop_practice.exception.NotEnoughStockException;
 import jpabook.jpashop_practice.repository.OrderRepository;
-import org.aspectj.weaver.ast.Or;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,16 +34,9 @@ public class OrderServiceTest {
 	@Test
 	public void 상품주문() throws Exception{
 	    //given
-		Member member = new Member();
-		member.setName("회원1");
-		member.setAddress(new Address("서울", "강가", "123-123"));
-		em.persist(member);
+		Member member = createMember();
 
-		Book book = new Book();
-		book.setName("시골 JPA");
-		book.setPrice(10000);
-		book.setStockQuantity(10);
-		em.persist(book);
+		Book book = createBook("시골 JPA", 10000, 10);
 
 		int orderCount = 2;
 
@@ -60,31 +51,52 @@ public class OrderServiceTest {
 		assertEquals("주문 가격은 가격 * 수량이다.", 10000 * orderCount, getOrder.getTotalPrice());
 		assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8, book.getStockQuantity());
 	}
-	
+
 	@Test(expected = NotEnoughStockException.class)
 	public void 상품주문_재고수량초과() throws Exception{
 	    //given
-	    
-	    //when
-	    
+		Member member = createMember();
+		Item item = createBook("시골 JPA", 10000, 10);
+
+		int orderCount = 11; // 만약 재고 수량이 10개 이상이면 익셉션이 발생해야 한다.
+		//when
+	    orderService.order(member.getId(), item.getId(), orderCount);
 	    //then
+		fail("재고 수량 부족 예외가 발생해야 한다.");
 	}
 	
 	@Test
 	public void 주문취소() throws Exception{
 	    //given
-	    
-	    //when
-	    
-	    //then
+		Member member = createMember();
+		Book item = createBook("시골 JPA", 10000, 10);
+
+		int orderCount = 2;
+		Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+		// 주문 취소를 테스트 해야 하기 때문에 기본 절에서 주문 코드까지 들어가는게 맞다.
+		//when
+		orderService.cancelOrder(orderId);
+
+		//then
+		Order getOrder = orderRepository.findOne(orderId);
+		assertEquals("주문 취소시 상태는 CANCEL 이다.", OrderStatus.CANCEL, getOrder.getStatus());
+		assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야 한다.", 10, item.getStockQuantity());
 	}
-	
-	@Test
-	public void 상품주문_재고수량초과() throws Exception{
-	    //given
-	    
-	    //when
-	    
-	    //then
+
+	private Book createBook(String name, int price, int stockQuantity) {
+		Book book = new Book();
+		book.setName(name);
+		book.setPrice(price);
+		book.setStockQuantity(stockQuantity);
+		em.persist(book);
+		return book;
+	}
+
+	private Member createMember() {
+		Member member = new Member();
+		member.setName("회원1");
+		member.setAddress(new Address("서울", "강가", "123-123"));
+		em.persist(member);
+		return member;
 	}
 }
