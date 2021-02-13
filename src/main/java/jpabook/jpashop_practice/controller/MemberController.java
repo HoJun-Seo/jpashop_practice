f5610cd487a5b6043a3e772b6044c231572501ec
SeoHoJun <ghwns6659@naver.com>
2021-02-13 20:38:49 +0900

    SpringBoot_JPA Practice

--- src/main/java/jpabook/jpashop_practice/controller/MemberController.java
+++ src/main/java/jpabook/jpashop_practice/controller/MemberController.java
@@ -6,6 +6,7 @@ import jpabook.jpashop_practice.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -29,10 +30,14 @@ public class MemberController {
 	}
 
 	@PostMapping("/members/new") // url 경로는 똑같으나, get 은 Form 화면을 열어보고, post 는 데이터를 실제로 등록해주는 역할을 한다.
-	public String create(@Valid MemberForm form){ // MemberForm 객체가 파라미터로 넘어오게 된다.
+	public String create(@Valid MemberForm form, BindingResult result){ // MemberForm 객체가 파라미터로 넘어오게 된다.
 		// 여기서 파라미터로 넘어오는 객체에 유효성 검증을 해줄수 있다.(Validation)
 		// @Valid 어노테이션을 활용해주면 Form 에 있는 Spring 이 javax.validation 기능을 쓰는 것을 인지하게 된다.
 		// 그로인해 Form 에 있는 @NotEmpty 와 같은 유효성 검증 코드를 통해 Spring 에서 validation 기능을 제공해주게 된다.
+		if (result.hasErrors()){
+			return "members/createMemberForm"; // 에러가 있을 경우 해당 경로로 리턴
+			// 스프링이 BindingResult 를 화면까지 다 끌고 가준다.(Spring-thymeleaf 간 integrition(통합) 이 라이브러리를 통해 그만큼 잘 되어져 있다.)
+		}
 
 		Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
 
